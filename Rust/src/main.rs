0f7076e59d3bc7635b4ebb6bdc99882121c37167
Muneeb Sial <muneeburrehmansial@032168@gmail.com>
2025-11-22 21:36:02 +0500

    _27_remove_element.rs completed

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,8 +1,9 @@
 // mod _1_two_sum;
 // mod _20_valid_parentheses;
-mod _3_longest_substring_without_repeating_characters;
+// mod _3_longest_substring_without_repeating_characters;
+mod _27_remove_element;
 // mod _28_find_the_index_of_the_first_occurrence_in_a_string;
 fn main() {
-    let result = _3_longest_substring_without_repeating_characters::Solution::length_of_longest_substring(String::from("thykwuersqfntjbmudrpexnphaovsihlrfxpbehuumjpxkctptitqvkxvavpicbbiarleyfamoqjaucpnmkfhueeu"));
+    let result = _27_remove_element::Solution::remove_element(&mut vec![0,1,2,2,3,0,4,2],2);
     println!("{:?}", result);
 }
