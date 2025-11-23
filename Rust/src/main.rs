da979b424c22aece86674f7e14249ea8b3999338
Muneeb Sial <muneeburrehmansial@032168@gmail.com>
2025-11-23 10:48:55 +0500

    _704_binary_search.rs

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -2,9 +2,12 @@
 // mod _20_valid_parentheses;
 // mod _3_longest_substring_without_repeating_characters;
 // mod _27_remove_element;
-mod _26_remove_duplicates_from_sorted_array;
+// mod _26_remove_duplicates_from_sorted_array;
 // mod _28_find_the_index_of_the_first_occurrence_in_a_string;
+mod _704_binary_search;
+
+
 fn main() {
-    let result = _26_remove_duplicates_from_sorted_array::Solution::remove_duplicates(&mut vec![1,1,2]);
+    let result = _704_binary_search::Solution::search(vec![-1,0,3,5,9,12], 2);
     println!("{:?}", result);
 }
