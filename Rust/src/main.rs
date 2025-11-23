a06a9fd185d79ba06407abb2ba5e3d30338a547e
Muneeb Sial <muneeburrehmansial@032168@gmail.com>
2025-11-23 09:58:05 +0500

    26_remove_duplicates_from_sorted_array.rs

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,9 +1,10 @@
 // mod _1_two_sum;
 // mod _20_valid_parentheses;
 // mod _3_longest_substring_without_repeating_characters;
-mod _27_remove_element;
+// mod _27_remove_element;
+mod _26_remove_duplicates_from_sorted_array;
 // mod _28_find_the_index_of_the_first_occurrence_in_a_string;
 fn main() {
-    let result = _27_remove_element::Solution::remove_element(&mut vec![0,1,2,2,3,0,4,2],2);
+    let result = _26_remove_duplicates_from_sorted_array::Solution::remove_duplicates(&mut vec![1,1,2]);
     println!("{:?}", result);
 }
