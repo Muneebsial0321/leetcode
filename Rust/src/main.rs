b6013bf11201fa5659c2db2aab52bd5c5365a322
Muneeb Sial <muneeburrehmansial@032168@gmail.com>
2025-12-04 08:24:20 +0500

    14_longest_common_prefix.rs

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -6,9 +6,13 @@
 // mod _28_find_the_index_of_the_first_occurrence_in_a_string;
 // mod _704_binary_search;
 // mod _11_container_with_most_water;
-mod _136_single_number;
+// mod _136_single_number;
+mod _14_longest_common_prefix;
 
 fn main() {
-    let result = _136_single_number::Solution::single_number(vec![1, 2, 2]);
+    let result = _14_longest_common_prefix::Solution::longest_common_prefix(vec![
+        "ab".to_string(),
+        "a".to_string(),
+    ]);
     println!("{:?}", result);
 }
