423db81a853f2f5b1d604fe69fe9d6c4c4c548aa
Muneeb Sial <muneeburrehmansial@032168@gmail.com>
2025-12-04 08:59:50 +0500

    35_search_insert_position.rs

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,18 +1,6 @@
-// mod _1_two_sum;
-// mod _20_valid_parentheses;
-// mod _3_longest_substring_without_repeating_characters;
-// mod _27_remove_element;
-// mod _26_remove_duplicates_from_sorted_array;
-// mod _28_find_the_index_of_the_first_occurrence_in_a_string;
-// mod _704_binary_search;
-// mod _11_container_with_most_water;
-// mod _136_single_number;
-mod _14_longest_common_prefix;
+mod _35_search_insert_position;
 
 fn main() {
-    let result = _14_longest_common_prefix::Solution::longest_common_prefix(vec![
-        "ab".to_string(),
-        "a".to_string(),
-    ]);
+    let result = _35_search_insert_position::Solution::search_insert(vec![1,3,5,6],2);
     println!("{:?}", result);
 }
